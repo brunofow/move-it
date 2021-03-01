161ca74b1df4ee56302f17dedf77ef368afc1563
brunofow <brunocamposfow@gmail.com>
2021-03-01 16:29:33 -0300

    Unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import '../styles/global.css'
 import '../styles/global.css'
 import type { AppProps } from 'next/app';
 
-import { ChallengesProvider } from '../contexts/ChallengeContext'
-
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
